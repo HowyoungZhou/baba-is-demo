db97092fb80b95f01b69eea72e22c329f07c1c56
Howyoung <howyoung3@gmail.com>
2020-07-14 23:50:26 +0800

    Exclude sleeping animation

--- src/character_entity.h
+++ src/character_entity.h
@@ -9,8 +9,8 @@ class CharacterEntity : public DirectionalEntity {
 GODOT_SUBCLASS(CharacterEntity, DirectionalEntity)
 public:
     static void _register_methods() {
-        godot::register_property<CharacterEntity, int>("Animation Index", &CharacterEntity::animation_index_, 1);
-        godot::register_property<CharacterEntity, int>("Animation Count", &CharacterEntity::animation_count_, 5);
+        godot::register_property<CharacterEntity, int>("Animation Index", &CharacterEntity::animation_index_, 0);
+        godot::register_property<CharacterEntity, int>("Animation Count", &CharacterEntity::animation_count_, 4);
     }
 
     void next_animation() {
@@ -20,8 +20,8 @@ public:
     }
 
 protected:
-    int animation_index_ = 1;
-    int animation_count_ = 5;
+    int animation_index_ = 0;
+    int animation_count_ = 4;
 
     virtual godot::String get_animation_name() const override {
         return DirectionalEntity::get_animation_name() + (std::stringstream() << animation_index_).str().c_str();
