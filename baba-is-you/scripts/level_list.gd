11264e71439ec9ef0bdf79263c98556641fe0711
Howyoung <howyoung3@gmail.com>
2020-07-25 23:41:56 +0800

    Add levels in The Lake

--- baba-is-you/scripts/level_list.gd
+++ baba-is-you/scripts/level_list.gd
@@ -1,16 +1,29 @@
 extends ItemList
 
 var levels = [
-	'Baba Is You',
-	'Where Do I Go?',
-	'Now What Is This?',
-	'Out Of Reach',
-	'Still Out Of Reach',
-	'Volcano',
-	'Off Limits',
-	'Grass Yard',
-	'Slideshow',
-	'Fragile Existence',
+	# Map
+	'Baba Is You', # 00
+	'Where Do I Go?', # 01
+	'Now What Is This?', # 02
+	'Out Of Reach', # 03
+	'Still Out Of Reach', # 04
+	'Volcano', # 05
+	'Off Limits', # 06
+	'Grass Yard', # 07
+	# The Lake
+	'Icy Waters', # 01
+	'Turns', # 02
+	# 'Affection', # 03
+	'Pillar Yard', # 04
+	'Brick Wall', # 05
+	'Lock', # 06
+	'Novice Locksmith', # 07
+	'Locked In', # 08
+	'Changeless', # 09
+	'Two Doors', # 10
+	'Jelly Throne', # 11
+	# 'Crab Storage', # 12
+	'Burglary', # 13
 ]
 
 # Called when the node enters the scene tree for the first time.
